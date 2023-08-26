8e644374dd702d086f79b0afea3d9f074c223a0c
patamarcelo <patamarcelo@gmail.com>
2023-08-26 09:55:10 -0300

    first changes on app

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import StartGameScreen from "./screens/StartGameScreen";
+
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.textStyle}>
 				Open up App.js to start working on your app!
 			</Text>
-			<StatusBar style="auto" />
+			<StartGameScreen />
 		</View>
 	);
 }
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View, Button } from "react-native";
+
+const StartGameScreen = () => {
+	return (
+		<View>
+			<TextInput />
+			<Button title="first Button" />
+		</View>
+	);
+};
+
+export default StartGameScreen;
